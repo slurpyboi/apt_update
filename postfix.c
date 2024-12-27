c9eb47eb41e2820589c1c080644d07a37562d66b
Solemon Nasandra <bzechsexio@gmail.com>
2024-12-27 22:27:39 +0530

    Add files via upload

---
+++ postfix.c
@@ -0,0 +1,60 @@
+#include <stdio.h> 
+#include<stdlib.h>  
+#include<ctype.h>  
+#include <math.h> 
+ 
+ 
+int stack[20]; 
+int  top =-1; 
+char postfix[20]; 
+ 
+void push(int item) 
+{   
+    stack[++top]=item; 
+} 
+ 
+int pop() 
+{   
+    return(stack[top --]); 
+} 
+ 
+ 
+void postfixeval( ) 
+{ 
+   int i=0,op1,op2,result; 
+   printf("\n\nEnter the postfix Expression:");  
+   gets(postfix); 
+    
+   for(i=0;postfix[i]!='\0';i++) 
+   { 
+        if(isdigit(postfix[i])) 
+               push(postfix[i]-'0'); 
+       else 
+       {  
+          op2=pop(); 
+          op1=pop(); 
+ 
+          switch(postfix[i]) 
+         { 
+                case'+':   push(op1+op2);  
+                                 break; 
+                case '-':   push(op1-op2);  
+                                 break; 
+                case'*':   push(op1*op2);  
+                                 break; 
+                case'/':   push(op1/op2);  
+                                 break; 
+                case'%':push(op1%op2); 
+                                break; 
+                case'^':push(pow(op1,op2)); 
+                               break; 
+          }   //switch end 
+      }//for end 
+  } 
+     printf("\n Given sufffix Expression n: %s\n", postfix);  
+     printf("\nResult after Evaluation:%d\n",stack[top]); 
+}
+void main()
+{
+        postfixeval();
+}
