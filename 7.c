5bdf833aa3c59aca923402eef0ba9d1f2f899ec1
Solemon Nasandra <bzechsexio@gmail.com>
2024-12-27 23:45:39 +0530

    7.c

--- 7.c
+++ 7.c
@@ -1,106 +1,107 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-#define MAX 100
-
-int stack[MAX];
-int top = -1;
-
-void push(int data) {
-    if (top >= MAX - 1) {
-        printf("Stack Overflow\n");
-    } else {
-        top++;
-        stack[top] = data;
-        printf("%d pushed to stack\n", data);
+struct node{
+     char usn[10];
+     char name[10];
+     char branch[10];
+     int sem;
+     char pno[10];    
+     struct node * next;
+};
+struct node * first=NULL;
+struct node *temp,*p,*q;
+void  nodecreate(){
+        temp=(struct node*)malloc(sizeof(struct node));
+        printf("\n EnterUSN:");
+        scanf("%s", temp->usn);
+        printf("\nEnter Name:");
+        scanf("%s",temp->name);
+        printf("\nEnter Branch:");
+        scanf("%s",temp->branch);
+        printf("\nEnter Semester:");
+        scanf("%d",&temp->sem);
+        printf("\nEnter phone number:");
+        scanf("%s",temp->pno);
+        temp->next=NULL;
+}
+void insertfront(){
+    nodecreate();
+    if(first==NULL)
+          first=temp;
+    else{
+        temp->next=first;
+        first=temp;
     }
 }
-
-int pop() {
-    if (top < 0) {
-        printf("Stack Underflow\n");
-        return -1;
-    } else {
-        int data = stack[top];
-        top--;
-        return data;
+void insertend(){
+      nodecreate();
+      if(first==NULL)
+              first=temp;
+      else
+      {
+        q=first;
+        while(q->next!=NULL)
+                q=q->next;
+        q->next=temp;
+      }
+}
+void deletefront(){
+    if(first==NULL)
+              printf("EMPTY LIST \n");
+    else{
+            q=first;
+            first=first->next;
+           printf("\n Student deleted is %s ",q->usn);
+           free(q);
     }
 }
-
-void display() {
-    if (top < 0) {
-        printf("Stack is Empty\n");
-    } else {
-        printf("Stack: ");
-        for (int i = top; i >= 0; i--) {
-            printf("%d ", stack[i]);
+void deleteend(){
+    if(first==NULL)
+             printf("THE LIST IS EMPTY. \n");
+    else  {
+           p=q;
+           q=first;
+            while(q->next!=NULL)
+         {
+                  p=q;
+                  q=q->next;
+         }
+              
+                p->next=NULL;
+                printf("\n Student deleted is %s",q->usn);
+                free(q); 
+   }
+}
+void display(){
+        if(first==NULL)
+               printf("\n LIST IS EMPTY \n");
+        else{
+            printf("\n -------------STUDENT DETAILS----------------\n");
+             for(q=first; q!=NULL;q=q->next)
+                      printf("%s\t%s\t%s\t%d\t%s------->",q->usn,q->name,q->branch,q->sem,q->pno);
         }
-        printf("\n");
-    }
 }
-
-int isPalindrome(char *str) {
-    int len = strlen(str);
-    for (int i = 0; i < len / 2; i++) {
-        push(str[i]);
-    }
-
-    int mid = (len % 2) ? (len / 2) + 1 : (len / 2);
-
-    for (int i = mid; i < len; i++) {
-        int popped = pop();
-        if (popped != str[i]) {
-            return 0; // Not Palindrome
-        }
-    }
-
-    return 1; // Palindrome
-}
-
-int main() {
-    int choice, data;
-    char str[MAX];
-
-    while (1) {
-        printf("\nStack Operations\n");
-        printf("1. Push\n");
-        printf("2. Pop\n");
-        printf("3. Check Palindrome\n");
-        printf("4. Display\n");
-        printf("5. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-
-        switch (choice) {
-            case 1:
-                printf("Enter data to push: ");
-                scanf("%d", &data);
-                push(data);
-                break;
-            case 2:
-                data = pop();
-                if (data != -1) {
-                    printf("Popped element: %d\n", data);
-                }
-                break;
-            case 3:
-                printf("Enter string: ");
-                scanf("%s", str);
-                if (isPalindrome(str)) {
-                    printf("%s is a palindrome\n", str);
-                } else {
-                    printf("%s is not a palindrome\n", str);
-                }
-                break;
-            case 4:
-                display();
-                break;
-            case 5:
-                exit(0);
-            default:
-                printf("Invalid Choice\n");
-        }
-    }
-    return 0;
+int main(){
+        int  choice,n,i;
+        while(1)
+        {
+            printf("\n\n-----SINGLY LINKED LIST MENU \n");
+            printf("1: CREATE\n 2: DISPLAY\n 3: INSERT AT END\n 4: INSERT AT FRONT\n 5: DELETE AT END\n 6: DELETE FROM FRONT\n 7: EXIT\n Enter your choice\n");
+            scanf("%d",&choice);
+            switch(choice)
+           {
+                    case 1:printf("Enter number of students:\n"); scanf("%d",&n);
+                            for(i=1;i<=n;i++)
+                                insertfront();
+                            printf("\n LIST CREATED\n"); break;
+                    case 2:  display();  break;        
+                    case 3:insertend();  break;         
+                    case 4:insertfront();    break;        
+                    case 5: deleteend();break;        
+                    case 6: deletefront();break;
+                    case 7: exit(0);
+                    default:printf("Invalid Choice\n");
+           } 
+    }               
 }
