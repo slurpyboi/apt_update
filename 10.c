3c21dc5203e9fefba94e75b922e2aff18ef3b37a
Solemon Nasandra <bzechsexio@gmail.com>
2024-12-30 01:44:27 +0530

    10.c

--- 10.c
+++ 10.c
@@ -3,32 +3,23 @@
 struct node
 {
       int data;
-      struct node *right; 
-      struct node *left;
+      struct node *right,*left;
 };
-
 struct node *root=NULL;
 struct node *found;
-struct node *  create(struct node *root, int  item)
-{
-    if (root == NULL)
-    {
+struct node *  create(struct node *root, int  item){
+    if (root == NULL){
         root=(struct node *)malloc(sizeof(struct node));
-        root->left = NULL;
-        root->right = NULL;
+        root->left = root->right = NULL;
         root->data = item;
     }
     else if (item < root->data)
          root->left = create(root->left, item);
     else 
          root->right = create(root->right, item);
-   
-         
-    return root;
+   return root;
 }
-
-void inorder(struct node * root)
-{
+void inorder(struct node * root){
     if (root != NULL)
     {
         inorder(root->left);
@@ -36,9 +27,7 @@ void inorder(struct node * root)
         inorder(root->right);
     }
 }
-
-void preorder(struct node * root)
-{
+void preorder(struct node * root){
     if (root != NULL)
     {
         printf(" %d ", root->data); 
@@ -46,9 +35,7 @@ void preorder(struct node * root)
         preorder(root->right);
     }
 }
-
-void postorder(struct node *root)
-{
+void postorder(struct node *root){
     if (root != NULL)
     {
         postorder(root->left); 
@@ -56,10 +43,7 @@ void postorder(struct node *root)
         printf(" %d ", root->data);
     }
 }
-
-
-struct node *search(struct node* root, int  key)
-{
+struct node *search(struct node* root, int  key){
     if(root==NULL)                               /* Element is not found */
          return NULL;
     if(key > root->data)                       /* Search in the right sub tree. */
@@ -69,10 +53,9 @@ struct node *search(struct node* root, int  key)
     else                                                 /* Element Found */
           return root;
 }
-
 void main()
 {
-    int choice, item, n, i,key;
+    int choice, item, n,i,key;
     while(1)
     {
         printf("\n---Binary Search Tree Operations---\n");
@@ -91,42 +74,33 @@ void main()
             case 1: printf("\n\nBST for How Many Nodes? : ");
                          scanf("%d", &n);
                          for (i=1;i<=n;i++)
-                        {
-                             printf("\nEnter the Data for Nodes");
-                             scanf("%d",&item);
-                             root=create(root, item);
-                        }
-                         printf("\nBST created!!\n");
-                         break;
-
+                            {
+                                printf("\nEnter the Data for Nodes");
+                                scanf("%d",&item);
+                                root=create(root, item);
+                            }
+                         printf("\nBST created!!\n");      break;
             case 2: printf("\n BST Traversal in INORDER \n");
-                          inorder(root);
-                          break;
+                          inorder(root);     break;
 
             case 3: printf("\n BST Traversal in PREORDER \n");
-                          preorder(root);
-                                  break;
-
+                          preorder(root);   break;
             case 4: printf("\n BST Traversal in POSTORDER \n");
-                          postorder(root);
-                          break;            
+                          postorder(root);  break;            
                                                            
-           case 5: printf("Enter node to be searched:\n"); 
+            case 5: printf("Enter node to be searched:\n"); 
                           scanf("%d",&key);
                           found=search(root,key); 
-                         if(found==NULL)
-                         {
-                                printf("Search Unsuccessful.\n");
-                                 break;
-                         }
-                       if(found->data==key)
+                          if(found==NULL)
+                            {
+                                    printf("Search Unsuccessful.\n");
+                                    break;
+                            }
+                          if(found->data==key)
                                printf("Search Successful. Node found in BST\n");                     
-                        break;
-
-         case 6: exit(0);
-
-        default: printf("\n\nInvalid Option !!! Try Again !! \n\n");
-                
+                          break;
+            case 6: exit(0);
+        default: printf("\n\nInvalid Option !!! Try Again !! \n\n");           
        }
    }
    free(root);
